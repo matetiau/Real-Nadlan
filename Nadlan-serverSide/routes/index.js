46244b623d644c84496b4c4813233dc7ebf03e2b
matetiau <matekamer@gmail.com>
2018-08-28 16:11:15 +0300

    buiding index.html server side fore rendering

--- Nadlan-serverSide/routes/index.js
+++ Nadlan-serverSide/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  fs.readFile('./index.html', null, function(error, data){
+    if(error){
+      res.writeHead(404);
+      res.write('File not found');
+    } else {
+      res.write(data);
+    }
+    res.end();
+ 
 });
 
 module.exports = router;
