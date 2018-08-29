97301dce6b7e5a7339a6b0926dc36f933f8a74a8
matetiau <matekamer@gmail.com>
2018-08-29 17:38:33 +0300

    more routes

--- Nadlan-serverSide/routes/index.js
+++ Nadlan-serverSide/routes/index.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const routes = require('./api')
+
 /* GET home page. */
 router.use(express.static('public'));
 router.use(express.static('routes/Html-files'));
 
 router.use(express.json());
-router.use(routes);
+router.use('/api', require('./api'));
 
 
 router.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
- 
+  
+console.log(process.env.port);
 });
-
 
 
 
